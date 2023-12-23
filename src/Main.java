755e1ad2d33989c3b2b6c0d182122b7a97401a96
iceedoc <paashtetik@gmail.com>
2023-12-24 00:08:35 +0300

    operation

---
+++ src/Main.java
@@ -0,0 +1,112 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age <= 15) {
+        System.out.println("Ты совершеннолетний");
+        } else {
+        System.out.println("Ты еще не совершеннолетний,нужно подождать");}
+
+        int withAHat = -5;
+        if (withAHat <= 5) {
+        System.out.println("Лучше надеть шапку");}
+        else {
+            System.out.println("Шапку можно не надевать");
+        }
+
+        int speed = 60;
+        if (speed <= 58) {
+            System.out.println("Придется заплатить штраф");}
+        else {
+            System.out.println("Можно ехать");
+        }
+        int a = 2;
+        int b =6;
+        if (a >= 2 && b <= 6) {
+            System.out.println("Тебе нужно ходить в сад");
+        }
+        int c = 7;
+        int d = 17;
+        if (c>=7 && d <= 17) {
+            System.out.println("Тебе нужно ходить в школу");
+        }
+        int e = 18;
+        int g = 24;
+        if (e >= 18 && g<= 24) {
+            System.out.println("Тебе нужно ходить в университет");
+        }
+        int h = 24;
+        if (h <= 22) {
+            System.out.println("Тебе нужно на работу");
+        }
+        else {
+            System.out.println("Тебе рано еще на работу");
+        }
+        int child = 5;
+        if (child < 5){
+            System.out.println("Может кататься");
+        }
+        else {
+            System.out.println("Не может кататься");
+        }
+        int child1 = 14;
+        if (child >= 5 && child1 <= 14){
+            System.out.println("Может кататься в сопровождении взрослого");
+        }
+        int cnild2 = 14;
+        if (cnild2 <= 18) {
+            System.out.println("Можно без сопровождения");
+        }
+        else {
+            System.out.println("С сопровождением");
+        }
+
+        int train = 102;
+        int sedentary = 60;
+        if (sedentary >= 42) {
+            System.out.println("Сидячие места");
+        }
+        else {
+            System.out.println("Стоячие места");
+        }
+        if (train <= 102 && sedentary <=60 ){
+            System.out.println("Есть стоячие места");
+        }
+
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        if (one < two && three > two) {
+            System.out.println("Большое число");
+        }
+
+
+
+        System.out.println("ЧУВСТВУЮ ЧТО НЕ ПРАВИЛЬНО ВСЕ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
